4995da2a94ff659bdecc257a5fa7d5d0bb8904b7
Anthony W. Juckel <ajuckel@gmail.com>
2010-12-11 17:21:03 -0600

    Updated test application.

--- src/main/java/net/juckel/osgijrubytest/Application.java
+++ src/main/java/net/juckel/osgijrubytest/Application.java
@@ -1,20 +1,47 @@
 package net.juckel.osgijrubytest;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngineFactory;
+import javax.script.SimpleScriptContext;
 
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
-import org.jruby.embed.ScriptingContainer;
+import org.jruby.Ruby;
+import org.jruby.embed.jsr223.JRubyEngine;
+import org.jruby.embed.jsr223.JRubyEngineFactory;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
 
 public class Application implements IApplication {
 
 //	@Override
 	public Object start(IApplicationContext context) throws Exception {
-		InputStream stream = this.getClass().getResourceAsStream("/src/main/ruby/simple.rb");
-		ScriptingContainer container = new ScriptingContainer();
-		container.setClassLoader(this.getClass().getClassLoader());
-		container.runScriptlet(stream, "simple.rb");
-		return Integer.valueOf(0);
+		JRubyEngineFactory fact = new JRubyEngineFactory();
+//		fact.setClassLoader(this.getClass().getClassLoader());
+		JRubyEngine engine = (JRubyEngine)fact.getScriptEngine();
+		// engine.getContext().setWriter(new OutputStreamWriter(System.out));
+		//engine.getBindings(ScriptContext.ENGINE_SCOPE).put("foo", this.getClass().getClassLoader());
+		Bindings bindings = engine.getBindings(ScriptContext.ENGINE_SCOPE); 
+		bindings.put("foo", this.getClass().getClassLoader());
+		bindings.put("out", System.out);
+		try {
+			InputStream stream = this.getClass().getResourceAsStream("/src/main/ruby/simple.rb");
+			// URL jirb = b.getEntry("/META-INF/jruby.home/bin/jirb_swing");
+	//		engine.eval(new InputStreamReader(jirb.openStream()));
+			Object returnValue =  engine.eval(new InputStreamReader(stream), bindings);
+	//		System.out.printf("Value: %s; %s", returnValue.getClass().getName(), returnValue.toString());
+			return Integer.valueOf(0);
+		} catch ( Exception e ) {
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 //	@Override
--- src/main/ruby/simple.rb
+++ src/main/ruby/simple.rb
@@ -1,6 +1,10 @@
 require 'java'
-
-import 'net.juckel.osgijrubytest.StupidClass'
-
+StupidClass = JavaUtilities.create_proxy_class(
+  'StupidClass',
+  JavaUtilities.get_java_class('net.juckel.osgijrubytest.StupidClass', $foo),
+  JavaUtilities.get_proxy_or_package_under_package('net.juckel.osgijrubytest', $foo))
+puts "What is StupidClass? #{StupidClass.class}, #{StupidClass}"
 c = StupidClass.new
-puts "From ruby #{1+1}; #{c.stuff}"
+puts "Value #{c.stuff}"
+
+1
